882b2b48c18cb0e8a427e0d4483e4f16dce32cb9
shauryabit2k18 <shaurya.r.sinha2000@gmail.com>
2019-12-31 12:38:04 +0530

    done

--- TensorFlow Deployment/Course 1 - TensorFlow-JS/Week 2/Exercise/fashion-script_exercise.js
+++ TensorFlow Deployment/Course 1 - TensorFlow-JS/Week 2/Exercise/fashion-script_exercise.js
@@ -14,48 +14,49 @@ function getModel() {
     // many layers, filters, and neurons as you like.  
     // HINT: Take a look at the MNIST example.
     model = tf.sequential();
-    
-    // YOUR CODE HERE
-    
-    
+
+    model.add(tf.layers.conv2d({inputShape: [28 , 28 , 1] , kernelSize: 3 , filters: 8 , activation: 'relu'}));
+    model.add(tf.layers.maxPooling2d({poolSize: [2 , 2]}));
+    model.add(tf.layers.conv2d({filters: 16 , kernelSize: 3 , activation: 'relu'}));
+    model.add(tf.layers.maxPooling2d({poolSize: [2 , 2]}));
+    model.add(tf.layers.flatten());
+    model.add(tf.layers.dense({units: 128 , activation: 'softmax'}));
+    model.add(tf.layers.dense({units: 10 , activation: 'softmax'}));
+
     // Compile the model using the categoricalCrossentropy loss,
     // the tf.train.adam() optimizer, and accuracy for your metrics.
-    model.compile(// YOUR CODE HERE);
-    
+    model.compile({optimizer: tf.train.adam() , loss: 'categoricalCrossentropy' , metrics: ['accuracy']});
+
     return model;
 }
 
 async function train(model, data) {
         
     // Set the following metrics for the callback: 'loss', 'val_loss', 'acc', 'val_acc'.
-    const metrics = // YOUR CODE HERE    
+    const metrics = ['loss' , 'val_loss' , 'acc' , 'val_acc'];
+    const container = {name: 'Model Training' , styles: {height: '1000px' }};
+    const fitCallbacks = tfvis.show.fitCallbacks(container , metrics);
 
-        
-    // Create the container for the callback. Set the name to 'Model Training' and 
-    // use a height of 1000px for the styles. 
-    const container = // YOUR CODE HERE   
-    
-    
-    // Use tfvis.show.fitCallbacks() to setup the callbacks. 
-    // Use the container and metrics defined above as the parameters.
-    const fitCallbacks = // YOUR CODE HERE
-    
     const BATCH_SIZE = 512;
     const TRAIN_DATA_SIZE = 6000;
     const TEST_DATA_SIZE = 1000;
-    
-    // Get the training batches and resize them. Remember to put your code
-    // inside a tf.tidy() clause to clean up all the intermediate tensors.
-    // HINT: Take a look at the MNIST example.
-    const [trainXs, trainYs] = // YOUR CODE HERE
 
-    
-    // Get the testing batches and resize them. Remember to put your code
-    // inside a tf.tidy() clause to clean up all the intermediate tensors.
-    // HINT: Take a look at the MNIST example.
-    const [testXs, testYs] = // YOUR CODE HERE
+    const [trainXs , trainYs] = tf.tidy(() =>{
+        const d = data.nextTrainBatch(TRAIN_DATA_SIZE);
+        return[
+            d.xs.reshape([TRAIN_DATA_SIZE , 28 , 28 , 1]),
+            d.labels
+        ];
+    });
 
-    
+    const [testXs , testYs] = tf.tidy(() =>{
+        const d = data.nextTestBatch(TEST_DATA_SIZE);
+        return [
+            d.xs.reshape([TEST_DATA_SIZE , 28 , 28 , 1]),
+            d.labels
+        ];
+    });
+  
     return model.fit(trainXs, trainYs, {
         batchSize: BATCH_SIZE,
         validationData: [testXs, testYs],
